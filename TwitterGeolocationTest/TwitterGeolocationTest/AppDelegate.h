cf696b7eac36d02e807cce9f6e2f7afb0a4f3913
dokun1 <david@okun.io>
2014-11-13 14:18:02 +0000

    initial project commit

---
+++ TwitterGeolocationTest/TwitterGeolocationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterGeolocationTest
+//
+//  Created by David Okun on 11/13/14.
+//  Copyright (c) 2014 okundotio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterGeolocationTest/TwitterGeolocationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterGeolocationTest
+//
+//  Created by David Okun on 11/13/14.
+//  Copyright (c) 2014 okundotio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
